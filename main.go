17cc468678ed78fd46c4b0dae26bce8de8fcf93c
gsmini <gsmini@sina.cn>
2023-02-22 23:46:15 +0800

    创建项目

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+
+	"github.com/go-micro-v4-demo/helloworld/handler"
+	pb "github.com/go-micro-v4-demo/helloworld/proto"
+
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/logger"
+
+)
+
+var (
+	service = "helloworld"
+	version = "latest"
+)
+
+func main() {
+	// Create service
+	srv := micro.NewService(
+	)
+	srv.Init(
+		micro.Name(service),
+		micro.Version(version),
+	)
+
+	// Register handler
+	if err := pb.RegisterHelloworldHandler(srv.Server(), new(handler.Helloworld)); err != nil {
+		logger.Fatal(err)
+	}
+	// Run service
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
